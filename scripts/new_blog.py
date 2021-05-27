73fee05ed187ac05af5570c6d80ce84ca8beb959
qifeng <qifeng@botpy.com>
2021-05-27 11:17:05 +0800

    fix: 修复脚本bug并优化脚本

--- scripts/new_blog.py
+++ scripts/new_blog.py
@@ -1,6 +1,7 @@
 import argparse
 import datetime
 
+import random
 import threading
 import time
 import logging
@@ -10,40 +11,41 @@ from pathlib import Path
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
 
 
 class InvalidSystemClock(Exception):
     """
     时钟回拨异常
     """
+
     pass
-
-
-# 64位ID的划分
-WORKER_ID_BITS = 5
-DATACENTER_ID_BITS = 5
-SEQUENCE_BITS = 12
-
-# 最大取值计算
-MAX_WORKER_ID = -1 ^ (-1 << WORKER_ID_BITS)  # 2**5-1 0b11111
-MAX_DATACENTER_ID = -1 ^ (-1 << DATACENTER_ID_BITS)
-
-# 移位偏移计算
-WOKER_ID_SHIFT = SEQUENCE_BITS
-DATACENTER_ID_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS
-TIMESTAMP_LEFT_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS + DATACENTER_ID_BITS
-
-# 序号循环掩码
-SEQUENCE_MASK = -1 ^ (-1 << SEQUENCE_BITS)
-
-# Twitter元年时间戳
-TWEPOCH = 1288834974657
 
 
 class IdWorker(object):
     """
     用于生成IDs
     """
+
+    # 64位ID的划分
+    WORKER_ID_BITS = 5
+    DATACENTER_ID_BITS = 5
+    SEQUENCE_BITS = 12
+
+    # 最大取值计算
+    MAX_WORKER_ID = -1 ^ (-1 << WORKER_ID_BITS)  # 2**5-1 0b11111
+    MAX_DATACENTER_ID = -1 ^ (-1 << DATACENTER_ID_BITS)
+
+    # 移位偏移计算
+    WOKER_ID_SHIFT = SEQUENCE_BITS
+    DATACENTER_ID_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS
+    TIMESTAMP_LEFT_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS + DATACENTER_ID_BITS
+
+    # 序号循环掩码
+    SEQUENCE_MASK = -1 ^ (-1 << SEQUENCE_BITS)
+
+    # Twitter元年时间戳
+    TWEPOCH = 1288834974657
 
     def __init__(self, datacenter_id, worker_id, sequence=0):
         """
@@ -53,11 +55,11 @@ class IdWorker(object):
         :param sequence: 其实序号
         """
         # sanity check
-        if worker_id > MAX_WORKER_ID or worker_id < 0:
-            raise ValueError('worker_id值越界')
+        if worker_id > self.MAX_WORKER_ID or worker_id < 0:
+            raise ValueError("worker_id值越界")
 
-        if datacenter_id > MAX_DATACENTER_ID or datacenter_id < 0:
-            raise ValueError('datacenter_id值越界')
+        if datacenter_id > self.MAX_DATACENTER_ID or datacenter_id < 0:
+            raise ValueError("datacenter_id值越界")
 
         self.worker_id = worker_id
         self.datacenter_id = datacenter_id
@@ -81,11 +83,11 @@ class IdWorker(object):
 
         # 时钟回拨
         if timestamp < self.last_timestamp:
-            logging.error('时钟向后移动. 拒绝请求，直到 {}'.format(self.last_timestamp))
+            logging.error("时钟向后移动. 拒绝请求，直到 {}".format(self.last_timestamp))
             raise InvalidSystemClock
 
         if timestamp == self.last_timestamp:
-            self.sequence = (self.sequence + 1) & SEQUENCE_MASK
+            self.sequence = (self.sequence + 1) & self.SEQUENCE_MASK
             if self.sequence == 0:
                 timestamp = self._til_next_millis(self.last_timestamp)
         else:
@@ -93,8 +95,12 @@ class IdWorker(object):
 
         self.last_timestamp = timestamp
 
-        new_id = ((timestamp - TWEPOCH) << TIMESTAMP_LEFT_SHIFT) | (self.datacenter_id << DATACENTER_ID_SHIFT) | \
-                 (self.worker_id << WOKER_ID_SHIFT) | self.sequence
+        new_id = (
+            ((timestamp - self.TWEPOCH) << self.TIMESTAMP_LEFT_SHIFT)
+            | (self.datacenter_id << self.DATACENTER_ID_SHIFT)
+            | (self.worker_id << self.WOKER_ID_SHIFT)
+            | self.sequence
+        )
         return new_id
 
     def _til_next_millis(self, last_timestamp):
@@ -109,67 +115,104 @@ class IdWorker(object):
 
 class FileWorker:
     def __init__(
-        self, path, title_id, title, subtitle, author, catalog,
-        tags="python", description="",
-        date=datetime.date.today().strftime("%Y-%m-%d")
+        self,
+        path,
+        title_id,
+        title,
+        subtitle,
+        author,
+        photo_path,
+        catalog,
+        tags="python",
+        description="",
+        date=datetime.date.today().strftime("%Y-%m-%d"),
     ):
         if not path or not title_id or not title:
             logging.error(f"path: {path}, title_id: {title_id}, title: {title}")
-            raise KeyError("缺少必要信息, 请检查-p, -t参数是否传递")
+            raise KeyError("缺少必要信息, 至少使用 -t 指定文章名称")
         self.path = path
         self.title_id = title_id
         self.file_name = f"{date}-{title_id}"
         self.title = title
         self.subtitle = subtitle
         self.author = author
+        self.photo_path = photo_path
         self.catalog = catalog
         self.tags = tags.split(",")
         self.description = description
         self.date = date
 
+    @property
     def build_header_info(self):
 
-        tags = "\n".join([f"  - {val}" for val in self.tags])
+        header_list = [val.name for val in self.photo_path.iterdir()]
+        header_image = header_list[random.randint(0, len(header_list) - 1)]
 
         header = [
             "---",
             f"title: {self.title}",
-            f"subtitle: {self.subtitle}"
+            f"subtitle: {self.subtitle}",
             f"author: {self.author}",
             f"date: {self.date}",
-            f"header_img: {self.date}",
+            f"header_img: /img/in-post/header/{header_image}",
             f"catalog: {self.catalog}",
             "tags:",
-            f"{tags}",
+            "\n".join([f"  - {val}" for val in self.tags]),
             "---",
-            f"{self.description}",
-            "<!-- more -->"
+            f"\n{self.description}",
+            "\n<!-- more -->\n",
         ]
         return [f"{val}\n" for val in header]
 
     def wirte_file(self):
-        header = self.build_header_info()
-        with open(self.path / f"posts/{self.file_name}", "w+") as f:
-            f.writelines(header)
+        header = self.build_header_info
+        try:
+            with open(self.path / f"posts/{self.file_name}.md", "w+") as f:
+                f.writelines(header)
+        except Exception as e:
+            logger.error(f"无法写入文件: {e}")
 
-        with open(self.path / "slug_title.txt", "a+") as sl:
-            sl.write(f"{self.title_id}:{self.title}")
+        try:
+            with open(self.path / "slug_title.txt", "a+") as sl:
+                sl.write(f"{self.title_id}:{self.title}\n")
+        except Exception as e:
+            logger.error(f"文章索引未记录，请手动添加: {e}")
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='文章描述信息')
-    parser.add_argument("-t", "--title", type=str, default="")
-    parser.add_argument("-s", "--subtitle", type=str, default="文章暂存")
-    parser.add_argument("-a", "--author", type=str, default="systemime")
-    parser.add_argument("-c", "--catalog", type=bool, default=True)
-    parser.add_argument("-ts", "--tags", type=str, default="python", help="以,分割")
+if __name__ == "__main__":
+
+    """
+    python scripts/new_blog.py -t "mysql中一千个不用NULL的理由" -s "技术分享" -ts "sql" -d "MySQL中null对性能有非常大对影响，聊聊使用null的原因以及不用null的理由"
+    """
+
+    parser = argparse.ArgumentParser(description="文章描述信息")
+    parser.add_argument("-t", "--title", type=str, default="", help="文章姓名")
+    parser.add_argument("-s", "--subtitle", type=str, default="文章暂存", help="文章分类")
+    parser.add_argument("-a", "--author", type=str, default="systemime", help="作者")
+    parser.add_argument(
+        "-p",
+        "--photo",
+        type=str,
+        default=Path(__file__).resolve(strict=True).parent.parent
+        / "blog/.vuepress/public/img/in-post/header",
+        help="图片地址",
+    )
+    parser.add_argument("-c", "--catalog", type=bool, default=True, help="是否开启目录")
+    parser.add_argument("-ts", "--tags", type=str, default="python", help="多个tag以,分割")
     parser.add_argument("-d", "--description", type=str, default="", help="文章摘要")
     parser.add_argument(
-      "-p", "--path", type=str, default=Path(__file__).resolve(strict=True).parent / "blog", help="博客路径")
+        "-P",
+        "--path",
+        type=str,
+        default=Path(__file__).resolve(strict=True).parent.parent / "blog",
+        help="博客路径",
+    )
     args = parser.parse_args()
 
     worker = IdWorker(1, 1, 0)
     title_id = worker.get_id()
+
+    logger.info(f"get titile_id: {title_id}")
 
     f_worker = FileWorker(
         Path(args.path),
@@ -177,9 +220,12 @@ if __name__ == '__main__':
         args.title,
         args.subtitle,
         args.author,
+        Path(args.photo),
         args.catalog,
         tags=args.tags,
         description=args.description,
     )
 
-    print(args)
+    logger.warning(f"{datetime.datetime.now()} :: 正在创建")
+    f_worker.wirte_file()
+    logger.warning(f"{datetime.datetime.now()} :: 创建完成")
